2f177bcf5f5ac4d2247f478df4129b8fb62064e5
Daniel Alfaro R <daniel.alfaro@upr.edu>
2020-10-28 15:53:14 -0400

    Added page to search for tours by dropdown.

--- src/Components/Home/Home.js
+++ src/Components/Home/Home.js
@@ -13,69 +13,6 @@ import Hiking from "../../images/hiking.jpg"
 import Zipline from "../../images/zipline.jpg"
 
 class Home extends React.Component {
-  constructor(props) {
-    super(props);
-
-    this.state = {
-      results: [],
-    };
-
-    this.search = this.search.bind(this);
-  }
-
-  search() {
-    this.setState({
-      results: [
-        {
-          name: "Rincon Beach Adventure",
-          location: "Rincon, PR",
-          type: "Beaches",
-          guides: ["Sandra", "Diego"],
-          duration: "3hr/s",
-          meetingPlace:
-            "The light house, right at the stairs of the main entrance",
-          meetingTime: "12:00pm",
-          price: "$40 per person",
-          reviews: [{ author: "Arnaldo", stars: "5", comments: "" }],
-          id: 1,
-        },
-        {
-          name: "Beer Tour",
-          location: "Rincon, PR",
-          type: "Food and Drinks",
-          guides: ["Walter"],
-          duration: "2.5hr/s",
-          meetingPlace: "Rincon Beer Company at the Rincon Plaza",
-          meetingTime: "7:00pm",
-          price: "$15 per person",
-          reviews: [
-            { author: "John", stars: "5", comments: "" },
-            {
-              author: "Wanda",
-              stars: "5",
-              comments: "Definitely recommend !!",
-            },
-          ],
-          id: 2,
-        },
-        {
-          name: "River Adventure",
-          location: "Rincon, PR",
-          type: "Rivers",
-          guides: ["Joanna", "Mike"],
-          duration: "4.5hr/s",
-          meetingPlace: "Rincon Light House, by the main entrance",
-          meetingTime: "9:00am",
-          price: "$25 per person",
-          reviews: [
-            { author: "Jacobo", stars: "5", comments: "Great Guides!" },
-            { author: "Ari", stars: "5", comments: "" },
-          ],
-          id: 3,
-        },
-      ],
-    });
-  }
 
   render() {
     return (
@@ -84,8 +21,6 @@ class Home extends React.Component {
         <Nav />
         <h1 className="Title">ExplorePR</h1>
         <h2 className="TitleDescription">Puerto Rico is full of hidden gems waiting for you to explore them.</h2>
-        <Search search={this.search} />
-        <SearchResults results={this.state.results} />
       </div>
       <div className="PlanAdventureDiv">
         <h1 className="PlanAdventureTitle">Plan your adventure</h1>
--- src/Components/Search/Search.js
+++ src/Components/Search/Search.js
@@ -1,24 +1,102 @@
 import React from "react";
 import "./Search.css";
-import { FaSearch } from "react-icons/fa";
+// import { FaSearch } from "react-icons/fa";
+import { FormGroup, FormControl, Placeholder } from "react-bootstrap";
 
 class Search extends React.Component {
   render() {
     return (
       <div className="SearchBox">
-        <FaSearch className="searchIcon" />
-        <input
-          type="text"
-          id="search"
-          placeholder="search by attraction or location..."
-        />
-        <button
+      <FormGroup id="searchForm">
+        <FormControl as="select" size="lg">
+          <option>Select By Municipality...</option>
+          <option>Adjuntas</option>
+          <option>Aguada</option>
+          <option>Aguadilla</option>
+          <option>Aguas Buenas</option>
+          <option>Aibonito</option>
+          <option>Arecibo</option>
+          <option>Arroyo</option>
+          <option>Añasco</option>
+          <option>Barceloneta</option>
+          <option>Barranquitas</option>
+          <option>Bayamón</option>
+          <option>Cabo Rojo</option>
+          <option>Caguas</option>
+          <option>Camuy</option>
+          <option>Canóvanas</option>
+          <option>Carolina</option>
+          <option>Cataño</option>
+          <option>Cayey</option>
+          <option>Ceiba</option>
+          <option>Ciales</option>
+          <option>Cidra</option>
+          <option>Coamo</option>
+          <option>Comerío</option>
+          <option>Corozal</option>
+          <option>Culebra</option>
+          <option>Dorado</option>
+          <option>Fajardo</option>
+          <option>Florida</option>
+          <option>Guayama</option>
+          <option>Guayanilla</option>
+          <option>Guaynabo</option>
+          <option>Gurabo</option>
+          <option>Guánica</option>
+          <option>Hatillo</option>
+          <option>Hormigueros</option>
+          <option>Humacao</option>
+          <option>Isabela</option>
+          <option>Jayuya</option>
+          <option>Juana Díaz</option>
+          <option>Juncos</option>
+          <option>Lajas</option>
+          <option>Lares</option>
+          <option>Las Marías</option>
+          <option>Las Piedras</option>
+          <option>Loiza</option>
+          <option>Luquillo</option>
+          <option>Manatí</option>
+          <option>Maricao</option>
+          <option>Maunabo</option>
+          <option>Mayagüez</option>
+          <option>Moca</option>
+          <option>Morovis</option>
+          <option>Naguabo</option>
+          <option>Naranjito</option>
+          <option>Orocovis</option>
+          <option>Patillas</option>
+          <option>Peñuelas</option>
+          <option>Ponce</option>
+          <option>Quebradillas</option>
+          <option>Rincón</option>
+          <option>Rio Grande</option>
+          <option>Sabana Grande</option>
+          <option>Salinas</option>
+          <option>San Germán</option>
+          <option>San Juan</option>
+          <option>San Lorenzo</option>
+          <option>San Sebastián</option>
+          <option>Santa Isabel</option>
+          <option>Toa Alta</option>
+          <option>Toa Baja</option>
+          <option>Trujillo Alto</option>
+          <option>Utuado</option>
+          <option>Vega Alta</option>
+          <option>Vega Baja</option>
+          <option>Vieques</option>
+          <option>Villalba</option>
+          <option>Yabucoa</option>
+          <option>Yauco</option>
+        </FormControl>
+      </FormGroup>
+      <button
           type="button"
           className="searchButton"
           onClick={this.props.search}
         >
           Enter
-        </button>
+      </button>
       </div>
     );
   }

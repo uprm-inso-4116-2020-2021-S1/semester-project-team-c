5c59de9ec102ee10badb952bf8f55db7a7869771
JDRamos15 <juan.ramos15@upr.edu>
2020-11-22 02:59:30 -0400

    Login works

--- src/Components/LogIn/LogIn.jsx
+++ src/Components/LogIn/LogIn.jsx
@@ -1,4 +1,4 @@
-import React, {component, useState} from "react";
+import React, { component, useState } from "react";
 import "./LogIn.css";
 import Nav from "../NavigationBar/NavigationBar";
 import Server from '../../services/serverRoutes';
@@ -7,17 +7,17 @@ import { login } from "../../services/authentication";
 const emailRegex = RegExp(/^([a-zA-Z0-9_\-\.]+)@([a-zA-Z0-9_\-\.]+)\.([a-zA-Z]{2,5})$/)
 const passwordRegex = RegExp(/^(?=.*\d)(?=.*[a-z])(?=.*[A-Z]).{4,20}$/)
 
-const [error, setError] = useState(null);
+// const [setError] = useState(null);
 
-const formValid = ({formErrors, ...rest}) => {
+const formValid = ({ formErrors, ...rest }) => {
   let valid = true;
-  
+
   Object.values(formErrors).forEach(val => {
-    val.length > 0 && (valid = false); 
+    val.length > 0 && (valid = false);
   });
 
   Object.values(rest).forEach(val => {
-    val=="" && (valid = false)
+    val == "" && (valid = false)
   });
 
   return valid;
@@ -25,14 +25,14 @@ const formValid = ({formErrors, ...rest}) => {
 var data = [];
 
 export class LogIn extends React.Component {
-  
+
   constructor(props) {
     super(props);
 
     this.state = {
       email: "",
       password: "",
-      formErrors:{
+      formErrors: {
         email: "",
         password: "",
       }
@@ -46,25 +46,31 @@ export class LogIn extends React.Component {
       password: this.state.password
 
     };
-    if(formValid(this.state)){
-        await Server.login(JSON.stringify(data)).then((response) => {
-          console.log(response);
-          login(response.body.access_token, response.body.email, response.body.role);
-        }).catch(error => {
-          if (error.response.status === 401) setError(error.response.data.message);
-          else setError("Something went wrong. Please try again later.");
-        });
+    if (formValid(this.state)) {
+      await Server.login(JSON.stringify(data)).then((response) => {
+        login(response.access_token, response.email, response.role);
+        if (response.role == 'customer') {
+          this.props.history.push('/customer');
+        } else if (response.role == 'guide') {
+          this.props.history.push('/guide');
+        }
+      });
+      // }).catch(error => {
+      //   if (error.response.status === 401) setError(error.response.data.message);
+      //   else setError("Something went wrong. Please try again later.");
+      // });
 
-    } else{console.error('FORM INVALID - DISPLAY ERROR MESSAGE');
+    } else {
+      console.error('FORM INVALID - DISPLAY ERROR MESSAGE');
     }
-  }; 
+  };
 
   handleChange = e => {
     e.preventDefault();
-    const{ name, value } = e.target;
+    const { name, value } = e.target;
     let formErrors = this.state.formErrors;
 
-    switch (name){
+    switch (name) {
       case 'email':
         formErrors.email = emailRegex.test(value) ? "" : "Invalid email Address";
         break;
@@ -72,58 +78,58 @@ export class LogIn extends React.Component {
         formErrors.password = passwordRegex.test(value) ? "" : "Password must be at least 4 characters, no more than 20 characters, and must include at least one upper case letter, one lower case letter, and one numeric digit.";
         break;
       default:
-        break; 
+        break;
     }
 
-    this.setState({formErrors, [name]: value})
+    this.setState({ formErrors, [name]: value })
   }
 
   render() {
 
-    const {formErrors} = this.state;
+    const { formErrors } = this.state;
 
     return (
       <React.Fragment>
-      <Nav/>
-      <div className="wrapper">
-        <div className="form-wrapper">
-          <h1>Login</h1>
-          <form onSubmit={this.handleSubmit} noValidate>
-            <div className="email">
-              <label htmlFor="email">Email</label>
-              <input 
-                className={formErrors.email.length > 0 ? "error" : null} 
-                placeholder ="email" 
-                type="email" 
-                name="email" 
-                noValidate
-                onChange={this.handleChange}
+        <Nav />
+        <div className="wrapper">
+          <div className="form-wrapper">
+            <h1>Login</h1>
+            <form onSubmit={this.handleSubmit} noValidate>
+              <div className="email">
+                <label htmlFor="email">Email</label>
+                <input
+                  className={formErrors.email.length > 0 ? "error" : null}
+                  placeholder="email"
+                  type="email"
+                  name="email"
+                  noValidate
+                  onChange={this.handleChange}
                 />
-            </div>
-            {formErrors.email.length > 0 && (
-                  <span className="errorMessage" >{formErrors.email}</span>
-                )}
-            <div className="password">
-              <label htmlFor="password">Password</label>
-              <input 
-                className={formErrors.password.length > 0 ? "error" : null} 
-                placeholder ="password" 
-                type="password" 
-                name="password" 
-                noValidate
-                onChange={this.handleChange}
+              </div>
+              {formErrors.email.length > 0 && (
+                <span className="errorMessage" >{formErrors.email}</span>
+              )}
+              <div className="password">
+                <label htmlFor="password">Password</label>
+                <input
+                  className={formErrors.password.length > 0 ? "error" : null}
+                  placeholder="password"
+                  type="password"
+                  name="password"
+                  noValidate
+                  onChange={this.handleChange}
                 />
-            </div>
-            {formErrors.password.length > 0 && (
-                  <span className="errorMessage" >{formErrors.password}</span>
-                )}
-            <div className="Submit">
-              <button type="submit">Log In</button> 
-              <small>Dont have an account?</small> <a href="/createaccount">Create Account</a>
-            </div>
-          </form> 
+              </div>
+              {formErrors.password.length > 0 && (
+                <span className="errorMessage" >{formErrors.password}</span>
+              )}
+              <div className="Submit">
+                <button type="submit">Log In</button>
+                <small>Dont have an account?</small> <a href="/createaccount">Create Account</a>
+              </div>
+            </form>
+          </div>
         </div>
-      </div>
       </React.Fragment>
     );
   }
--- src/Components/Profiles/CustomerProfile.js
+++ src/Components/Profiles/CustomerProfile.js
@@ -1,4 +1,4 @@
-import React, {component} from "react";
+import React, { component } from "react";
 import "./CustomerProfile.css";
 import Nav from "../NavigationBar/NavigationBar";
 import Container from 'react-bootstrap/Container'
@@ -10,41 +10,54 @@ import FacebookIcon from "../../images/FacebookIcon.png"
 import InstagramIcon from "../../images/InstagramIcon.png"
 import TwitterIcon from "../../images/TwitterIcon.png"
 import YoutubeIcon from "../../images/YoutubeIcon.png"
-
-export default class UserProfile extends React.Component{
-
-
-    render() {
-        return (
-          <React.Fragment>
-            <Nav /> 
-            <div>
-              <Image className="picDim d-block" src={Tongo} roundedCircle/>
-              <h1 className="Name">Name LastName</h1>
-              <h1 className="companyName">Company Name</h1>
-              <div className= "HeaderDiv">
-                <a href="/createaccount">Create Event</a>     
-              </div>
-              <Row className="justify-content-md-center">
-                <Col sm = {1} className="iconColPadding">
-                  <a href="https://www.facebook.com/Tongo_PR-1491833804253503">
-                  <Image className="picDim d-block mx-auto Icon" src={FacebookIcon}/>
-                  </a>
-                </Col>
-                <Col sm = {1} className="iconColPadding"><a href="https://www.instagram.com/tongo_pr/"><Image className="picDim d-block mx-auto Icon" src={InstagramIcon}/></a></Col>
-                <Col sm = {1} className="iconColPadding"><a href="https://twitter.com/tito824"><Image className="picDim d-block mx-auto Icon" src={TwitterIcon}/></a></Col>
-                <Col sm = {1} className="iconColPadding"><a href="https://www.youtube.com/channel/UCUHL6ynG35dZQAbrNfuLKAw"><Image className="picDim d-block mx-auto Icon" src={YoutubeIcon}/></a></Col>
-              </Row>
-              
-              
-            </div>
+// import { getUser, logout } from "../../services/authentication";
 
 
+// const user = getUser();
 
-          
-          </React.Fragment>
-        );
-      }
-    
+export default class UserProfile extends React.Component {
+
+  // constructor(props) {
+  //   super(props);
+  // };
+
+  // handleLogout = () => {
+  //   logout();
+  //   this.props.history.push('/login');
+  // }
+
+  render() {
+    return (
+      <React.Fragment>
+        <Nav />
+        <div>
+          <Image className="picDim d-block" src={Tongo} roundedCircle />
+          <h1 className="Name">Name LastName</h1>
+          <h1 className="companyName">Company Name</h1>
+          <div className="HeaderDiv">
+            <a href="/createaccount">Create Event</a>
+          </div>
+          {/* <input type="button" onClick={this.handleLogout} value="Logout" /> testing logout*/}
+          <Row className="justify-content-md-center">
+            <Col sm={1} className="iconColPadding">
+              <a href="https://www.facebook.com/Tongo_PR-1491833804253503">
+                <Image className="picDim d-block mx-auto Icon" src={FacebookIcon} />
+              </a>
+            </Col>
+            <Col sm={1} className="iconColPadding"><a href="https://www.instagram.com/tongo_pr/"><Image className="picDim d-block mx-auto Icon" src={InstagramIcon} /></a></Col>
+            <Col sm={1} className="iconColPadding"><a href="https://twitter.com/tito824"><Image className="picDim d-block mx-auto Icon" src={TwitterIcon} /></a></Col>
+            <Col sm={1} className="iconColPadding"><a href="https://www.youtube.com/channel/UCUHL6ynG35dZQAbrNfuLKAw"><Image className="picDim d-block mx-auto Icon" src={YoutubeIcon} /></a></Col>
+          </Row>
+
+
+        </div>
+
+
+
+
+      </React.Fragment>
+    );
+  }
+
 
 }
